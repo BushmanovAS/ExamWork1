7e16481239ab8e25a0e0e042981bce5e3a8b4783
Антон Бушманов <antonbusmanov@mac-mini-anton.local>
2020-06-28 16:50:01 +0300

    Initial Commit

---
+++ diplom1.6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diplom1.6
+//
+//  Created by Антон Бушманов on 28.06.2020.
+//  Copyright © 2020 Антон Бушманов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
